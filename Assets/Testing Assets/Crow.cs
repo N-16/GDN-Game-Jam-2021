55aa9d5dc315c5370a5bbc365460944db5fd7fe8
N-16 <53996390+N-16@users.noreply.github.com>
2021-08-06 16:50:24 +0530

    crow Animations and level design
    bruh

--- Assets/Testing Assets/Crow.cs
+++ Assets/Testing Assets/Crow.cs
@@ -6,6 +6,8 @@ public class Crow : MonoBehaviour
 {
     [SerializeField] LayerMask playerLayer, scareCrowLayer;
     [SerializeField] Animator crowAnimator;
+    [SerializeField] float flyToIdleTransitionTime = 0.25f;
+    [SerializeField] Animator crowSpriteAnimator;
     [SerializeField] CircleCollider2D crowTerritoryTile;
     [SerializeField] BoxCollider2D crowScaringRegion;
     [SerializeField] float comeDownCoolDown = 2.5f; 
@@ -15,11 +17,14 @@ public class Crow : MonoBehaviour
     private void Update() {
         if (Physics2D.OverlapBox(crowTerritoryTile.transform.position, crowTerritoryTile.bounds.size, 0f, playerLayer)) {
             AttackPlayer();
+            
         }
         if (!onFlight) {
             if (Physics2D.OverlapBox(crowScaringRegion.transform.position, crowScaringRegion.bounds.size, 0f, scareCrowLayer)) {
                 StopCoroutine(ComeDownCoroutine());
+                StopCoroutine(FlyToIdleTransition());
                 PlayAnimation(CrowAnimations.Fly);
+                crowSpriteAnimator.SetBool("flying", true);
                 onFlight = true;
             }
         }
@@ -32,6 +37,9 @@ public class Crow : MonoBehaviour
     }
 
     private void AttackPlayer() {
+        StopCoroutine(ComeDownCoroutine());
+        StopCoroutine(FlyToIdleTransition());
+        crowSpriteAnimator.SetBool("flying", true);
         PlayAnimation(CrowAnimations.Attack);
         GameManager.Instance.OnPlayerDeath();
     }
@@ -42,8 +50,15 @@ public class Crow : MonoBehaviour
 
     IEnumerator ComeDownCoroutine() {
         yield return new WaitForSeconds(comeDownCoolDown);
-        if (!Physics2D.OverlapBox(crowScaringRegion.transform.position, crowScaringRegion.bounds.size, 0f, scareCrowLayer))
+        if (!Physics2D.OverlapBox(crowScaringRegion.transform.position, crowScaringRegion.bounds.size, 0f, scareCrowLayer)) {
             PlayAnimation(CrowAnimations.ComeBack);
+            StartCoroutine(FlyToIdleTransition());
+        }
+    }
+
+    IEnumerator FlyToIdleTransition() {
+        yield return new WaitForSeconds(flyToIdleTransitionTime);
+        crowSpriteAnimator.SetBool("flying", false);
     }
 }
 
